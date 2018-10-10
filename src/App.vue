64e6f19347cf6716d785563c81ba8ae0f1f09b91
KahkashanT <43177809+KahkashanT@users.noreply.github.com>
2018-10-10 19:36:15 +0530

    commit2

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
-  <div id="app">
-    HIHello
+  <div >
+    <templateDemo></templateDemo>
+    <computedProperties></computedProperties>
   </div>
 </template>
 
 <script>
-
+import templateDemo from "./templateDemo.vue";
+import computedProperties from "./computedProperties.vue";
 export default {
   name: "App",
-  
+  components: {
+    templateDemo,
+    computedProperties
+  }
 };
 </script>
-
---
+++ src/templateDemo.vue
@@ -0,0 +1,30 @@
+<template>
+  <div >
+  <!--interpolation-->
+    <!--<span>Message: {{ msg }}</span>
+    <br>
+    <span v-once>This will never change: {{ msg }}</span>
+    <br>
+    <button v-on:click="msg='HI'">Change data</button>-->
+
+    <!--rawHTML-->
+    <!--<p>Using mustaches: {{ rawHtml }}</p>
+    <p>Using v-html directive: <span v-html="rawHtml"></span></p>-->
+
+    <!--Attributes-->
+    <!--<button v-bind:disabled="isButtonDisabled">Button</button>-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "templateDemo",
+  data() {
+    return {
+      msg: "Hello",
+      rawHtml: "<span style='color:red'>This should be red</span>",
+      isButtonDisabled: false
+    };
+  }
+};
+</script>
